1b9562afa64b7cb994ca9908c1522a18f6615ef0
singh26harshita <singh26.harshita@gmail.com>
2018-10-22 13:27:32 +0200

    refinement version

--- app/scripts/landingModule/landing.js
+++ app/scripts/landingModule/landing.js
@@ -20,26 +20,31 @@ function($scope,backendDataServices) {
    * champions for season 2005 to 2015
    */
   $scope.getWorldChampion = function(){
-    var list = []; 
-   
-    for( var season=2005 ; season<=2015 ; season++) {
-      //URL to get the world champion
-      var url = 'http://ergast.com/api/f1/'+ season +'/driverStandings/1/drivers.json' ;
+    //create year object
+    var yearObj = [{year:'2005'},{year:'2006'},{year:'2007'},{year:'2008'},{year:'2009'},
+                  {year:'2010'},{year:'2011'},{year:'2012'},{year:'2013'},
+                  {year:'2014'},{year:'2015'}];
+                      
+    $scope.WinnerList = [];
+    //loop through each year object to create a final list of all winners that will display on screen
+    angular.forEach(yearObj,function(obj){
+      var url = 'http://ergast.com/api/f1/'+ obj.year +'/driverStandings/1/drivers.json' ;
       backendDataServices.getBackendData(url)
       .then(function successCallBack(response){    
-        list.push(response.MRData.DriverTable);
-        if(list.length==11){
-            $scope.WinnerList = list;
-            //hiding the loading mask
-            $scope.hideMask=true ;
+        obj.winner= response.MRData.DriverTable;
+        $scope.WinnerList.push(obj.winner);
+        if(obj.winner.season==2015){
+          //when loop is on last season hide loading mask
+          $scope.hideMask = true ;
         }
       }).catch(function(error){
         console.log(error);
       });
-    }     
+      
+    });
   }
   
   //at controller init call the function to get list of world champions
   $scope.getWorldChampion();
-
-}])
+   
+}]);
